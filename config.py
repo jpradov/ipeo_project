a679c20d21c7adbff25908ecd02edd7e9e9657dc
Joao <joao.pradovieira@epfl.ch>
2023-11-05 13:34:14 +0100

    implement dataset class and demo of sample augmentation

--- config.py
+++ config.py
@@ -1,1 +1,4 @@
-PATH_TO_DATA = r"C:\data_repository\IPEO\project\dataset"  # path to your folder containing the images & labels folder as well as the train test split
+import torch 
+
+PATH_TO_DATA = r"dataset"  # path to your folder containing the images & labels folder as well as the train test split
+device = "cuda" if torch.cuda.is_available() else "cpu"
--- utils/data.py
+++ utils/data.py
@@ -2,7 +2,12 @@
 
 import numpy as np
 from skimage import exposure
-
+from torchgeo.datasets.geo import NonGeoDataset
+from torch.utils.data import Dataset
+import config
+import os
+import torch
+from skimage.io import imread
 
 def normalized_image(image):
     """
@@ -29,3 +34,50 @@ def normalized_image(image):
             image[:, :, k], in_range=(p_min, p_max), out_range=(0, 255)
         )
     return output.astype(np.uint8)
+
+class PlanetDataset(NonGeoDataset,Dataset):
+    def __init__(self, data_dir=config.PATH_TO_DATA, bands=[0,1,2,3], transform=None, target_transform=None):
+        """
+        Args:
+            data_dir (str): The root directory where the Planet dataset is stored.
+            bands (list(int)) : A list of band indexes to be used.
+            transform (callable, optional): A function/transform to apply to the data samples.
+            target_transform (callable, optional): A function/transform to apply to the target masks.
+        """
+        
+        self.root = data_dir
+        self.image_path = os.path.join(self.root, r"images")
+        self.mask_path = os.path.join(self.root, r"labels")
+        self.folder_data = os.listdir(self.image_path)
+        self.folder_mask = os.listdir(self.mask_path)
+        self.bands=bands
+        self.transform = transform
+        self.target_transform = target_transform
+
+
+
+    def __getitem__(self, index):
+        sample_path = os.path.join(self.image_path,self.folder_data[index])
+        mask_path = os.path.join(self.mask_path,self.folder_mask[index])
+
+        sample = torch.from_numpy(self.custom_loader(path=sample_path)[:, :, self.bands])
+        mask = torch.from_numpy(self.custom_loader(mask_path))
+
+        if self.transform is not None:
+            sample = self.transform(sample)
+
+        if self.target_transform is not None:
+            mask = self.target_transform(mask)
+
+        return sample, mask
+
+    def __len__(self):
+
+        return len(self.folder_data)
+    
+    def custom_loader(self,path):  
+        return imread(path).astype(np.int16) 
+        
+    def __str__(self):
+        
+        return f"PlanetDataset(root={self.root}, num_samples={len(self)}, transform={self.transform}, target_transform={self.target_transform})"
