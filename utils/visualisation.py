4bb78425f3ec0cbcb40e034b2e7251ebbbd2664e
serraj <mehdi.serraj@epfl.ch>
2023-12-24 13:30:20 +0100

    fixed dimensions

--- utils/visualisation.py
+++ utils/visualisation.py
@@ -9,20 +9,20 @@ from utils.data import normalized_image
 def visualise_image(image, label):
     """Function to show true color, false color and segmentation mask of an image."""
 
-    normalized = normalized_image(image)
+    normalized = normalized_image(image.squeeze().numpy())
 
     fig, axes = plt.subplots(
         nrows=1, ncols=3, sharex=True, sharey=True, figsize=(18, 8)
     )
-    axes[0].imshow(normalized[:, :, [0, 1, 2]])
+    axes[0].imshow(normalized[[0, 1, 2], :, :].transpose(1, 2, 0))
     axes[0].set_title("True Color Image")
     axes[0].axis("off")
 
-    axes[1].imshow(normalized[:, :, [3, 0, 1]])
+    axes[1].imshow(normalized[[1, 2, 3], :, :].transpose(1, 2, 0))
     axes[1].set_title("False Color Image (NINFR, RED, GREEN)")
     axes[1].axis("off")
 
-    axes[2].imshow(label)
+    axes[2].imshow(label.squeeze().numpy())
     axes[2].set_title("Segmentation Mask")
     axes[2].axis("off")
 
@@ -32,16 +32,16 @@ def visualise_image(image, label):
 def visualise_image_3_channels(image, label):
     """Function to show true color, false color and segmentation mask of an image."""
 
-    normalized = normalized_image(image)
+    normalized = normalized_image(image.squeeze().numpy())
 
     fig, axes = plt.subplots(
         nrows=1, ncols=2, sharex=True, sharey=True, figsize=(18, 8)
     )
-    axes[0].imshow(normalized[:, :, [0, 1, 2]])
+    axes[0].imshow(normalized[[0, 1, 2], :, :].transpose(1, 2, 0))
     axes[0].set_title("True Color Image")
     axes[0].axis("off")
 
-    axes[1].imshow(label)
+    axes[1].imshow(label.squeeze().numpy())
     axes[1].set_title("Segmentation Mask")
     axes[1].axis("off")
 
@@ -50,7 +50,7 @@ def visualise_image_3_channels(image, label):
 
 def plot_band_distribution(image):
     fig, axes = plt.subplots(1, 4, sharex=False, sharey=True, figsize=(14, 4))
-    for k in range(image.shape[2]):
-        band = image[:, :, k].flatten()
+    for k in range(image.shape[0]):
+        band = image[k, :, :].flatten()
         sns.histplot(band, ax=axes[k], kde=True)
         axes[k].set_title("Band {}".format(k + 1))
