4f6853cdc5414d46a755ddaffccb7af727ed6413
Luca-Andrea Sbicego <luca.sbicego@icloud.com>
2023-12-30 20:00:44 +0100

    reordering config file

--- config.py
+++
@@ -1,5 +0,0 @@
-import torch
-
-# path to your folder containing the images & labels folder as well as the train test split
-PATH_TO_DATA = r"C:\data_repository\IPEO\project\dataset"
-device = "cuda" if torch.cuda.is_available() else "cpu"
---
+++ config/config.py
@@ -0,0 +1,2 @@
+# path to your folder containing the images & labels folder as well as the train test split
+PATH_TO_DATA = r"." # CHANGE HERE
